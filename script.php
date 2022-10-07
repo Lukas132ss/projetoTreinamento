ec1a0fbf52b57886f9dc491a7da2f6f975226780
Lukas de Souza Santos <lukas.santos@onclick.com.br>
2022-10-07 11:46:25 -0300

    Atualizando validações do formulario

--- script.php
+++ script.php
@@ -9,14 +9,30 @@ $categorias[] = 'adulto';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-// var_dump($nome);
-// var_dump($idade);
+if(empty($nome)){
+    echo 'O nome não pode ser vazio.';
+    return;
+}
+
+if(strlen($nome) < 3) {
+    echo 'O nome deve conter mais de 3 caracteres.';
+    return;
+}
+
+if(strlen($nome) > 40){
+    echo 'O nome está muito extenso.';
+    return;
+}
+
+if(!is_numeric($idade)) {
+    echo 'Informe um numero para idade.'
+}
 
 if($idade >= 6 && $idade <= 12){
 	for($i = 0; $i <= count($categorias); $i++){
 		if($categorias[$i] = "infantil" ) {
 			echo "O nadador ".$nome." compete na categoria ".$categorias[$i].".";
-            return 0;
+            return;
 		}
 	}
 }
@@ -24,7 +40,7 @@ else if($idade >= 13 && $idade <=18){
 	for($i = 0; $i <= count($categorias); $i++){
 		if($categorias[$i] = 'adolescente') {
 			echo "O nadador ".$nome." compete na categoria ".$categorias[$i].".";
-            return 0;
+            return;
 		}
 	}
 }
@@ -32,7 +48,7 @@ else {
 	for($i = 0; $i <= count($categorias); $i++){
 		if($categorias[$i] = 'adulto') {
 			echo "O nadador ".$nome." compete na categoria ".$categorias[$i].".";
-            return 0;
+            return;
 		}
 	}
 }
